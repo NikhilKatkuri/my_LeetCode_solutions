55cd50b507d2e456ad5dcfbc08d7a62f54bcd400
NikhilKatkuri <knikhil07k@gmail.com>
2025-05-27 09:38:57 +0530

    easy Q-007 : 27-05-2025 , easy level

---
+++ Easy/007-Length-of-Last-Word.js
@@ -0,0 +1,27 @@
+// Leet code Math question number: 58.
+
+
+// Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+// A word is a maximal substring consisting of non-space characters only.
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+
+var lengthOfLastWord = function (s) {
+    // trims leading space from the string
+    let str = s.trim();
+    // using split function splitting the space and it become array
+    str = str.split(' ');
+    // length of array
+    const length = str.length;
+    // returns the lenght last positon of string in array
+    return str[length - 1].length;
+};
+
+
+lengthOfLastWord("Hello World");
+lengthOfLastWord("   fly me   to   the moon  ");
+lengthOfLastWord("luffy is still joyboy");
